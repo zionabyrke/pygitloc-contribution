aa8149ab3e846a2d05e3cc61d14b0d40e60ba6d9
zionabyrke <renzkirbyonia@gmail.com>
2026-01-22 15:56:12 +0800

    docs+build: readme modif+argparse usage

--- README.md
+++ README.md
@@ -7,4 +7,11 @@ Visualize your local Git contributions in Python Dulwich
 # then run
 pip install dulwich rich
 
+python main.py # help
+
+# add ALL repos in a folder
+python main.py --add <path/to/repo>
+
+# see contribution tiles
+python main.py --email <your_email@gmail.com>
 ```
--- sys/main.py
+++ sys/main.py
@@ -1,4 +1,4 @@
-import os
+import argparse
 from scanner import find_git_repos
 from storage import load_repos, save_repos
 from commits import collect_commits
@@ -6,22 +6,23 @@ from merge import merge_stats
 from intensity import intensity_level
 from heatmap import render_heatmap
 
-# test finding repos
-# pwd = os.getcwd()
-# print(pwd)
+parser = argparse.ArgumentParser()
+parser.add_argument("--add", "-a", help="Scan folder for Git repos")
+parser.add_argument("--email", "-e", help="Author email")
+args = parser.parse_args()
 
-directory = "C://Users//Renz//Documents"
-# print(*find_git_repos(directory), sep='\n')
+if args.add:
+    repos = find_git_repos(args.add)
+    save_repos(repos)
+    print(f"Added {len(repos)} repositories.")
 
-email = "renzkirbyonia@gmail.com"
-repo = "C://Users//Renz//Documents//CODECRAFTERS//codecrafters-shell-cpp"
-commits = collect_commits(repo, email)
+elif args.email:
+        stats = [
+            collect_commits(repo, args.email)
+            for repo in load_repos()
+        ]
+        merged = merge_stats(stats)
+        render_heatmap(merged)
 
-print(f"Repo: {repo}")
-if not repo:
-    print("None found")
-
-print(f"Found {len(commits)} commits", end='')
-# print(commits)
-
-heatmap = render_heatmap(commits)
+else:
+    parser.print_help()
